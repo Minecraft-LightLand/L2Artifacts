99cf4d1f515f482a12629e869bd775b221d47f42
lcy0x1 <arthurlcy0x1@gmail.com>
2023-01-09 16:35:57 -0800

    add recipes

--- src/main/java/dev/xkmc/l2artifacts/init/data/RecipeGen.java
+++ src/main/java/dev/xkmc/l2artifacts/init/data/RecipeGen.java
@@ -4,6 +4,9 @@ import dev.xkmc.l2artifacts.content.core.BaseArtifact;
 import dev.xkmc.l2artifacts.init.L2Artifacts;
 import dev.xkmc.l2artifacts.init.registrate.ArtifactItemRegistry;
 import dev.xkmc.l2artifacts.init.registrate.entries.SetEntry;
+import dev.xkmc.l2complements.init.L2Complements;
+import dev.xkmc.l2complements.init.registrate.LCItems;
+import dev.xkmc.l2library.base.recipe.ConditionalRecipeWrapper;
 import dev.xkmc.l2library.repack.registrate.providers.RegistrateRecipeProvider;
 import dev.xkmc.l2library.repack.registrate.util.DataIngredient;
 import dev.xkmc.l2library.repack.registrate.util.entry.ItemEntry;
@@ -72,6 +75,30 @@ public class RecipeGen {
 					craft(pvd, output, center, input, corner);
 				}
 			}
+		}
+
+		// conditionals
+		{
+			unlock(pvd, new ShapedRecipeBuilder(ArtifactItemRegistry.ITEM_STAT[2].get(), 1)::unlockedBy, LCItems.RESONANT_FEATHER.get())
+					.pattern("AAA").pattern("BCB").pattern("AAA")
+					.define('A', Items.GOLD_INGOT)
+					.define('B', LCItems.SOUL_FLAME.get())
+					.define('C', LCItems.RESONANT_FEATHER.get())
+					.save(ConditionalRecipeWrapper.mod(pvd, L2Complements.MODID));
+
+			unlock(pvd, new ShapedRecipeBuilder(ArtifactItemRegistry.ITEM_BOOST_MAIN[2].get(), 1)::unlockedBy, LCItems.FORCE_FIELD.get())
+					.pattern("ABA").pattern("BCB").pattern("ABA")
+					.define('A', Items.GOLD_INGOT)
+					.define('B', LCItems.STORM_CORE.get())
+					.define('C', LCItems.FORCE_FIELD.get())
+					.save(ConditionalRecipeWrapper.mod(pvd, L2Complements.MODID));
+
+			unlock(pvd, new ShapedRecipeBuilder(ArtifactItemRegistry.ITEM_BOOST_SUB[2].get(), 1)::unlockedBy, LCItems.EMERALD.get())
+					.pattern("ABA").pattern("BCB").pattern("ABA")
+					.define('A', Items.GOLD_INGOT)
+					.define('B', LCItems.CAPTURED_WIND.get())
+					.define('C', LCItems.EMERALD.get())
+					.save(ConditionalRecipeWrapper.mod(pvd, L2Complements.MODID));
 		}
 	}
 

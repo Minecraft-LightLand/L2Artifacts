997bd48769681b162fbeb785e89df52ada54b852
lcy0x1 <arthurlcy0x1@gmail.com>
2023-02-01 21:09:37 -0800

    use loot modifier

--- src/main/java/dev/xkmc/l2artifacts/init/data/loot/ArtifactGLMProvider.java
+++ src/main/java/dev/xkmc/l2artifacts/init/data/loot/ArtifactGLMProvider.java
@@ -1,6 +1,7 @@
 package dev.xkmc.l2artifacts.init.data.loot;
 
 import dev.xkmc.l2artifacts.init.L2Artifacts;
+import dev.xkmc.l2artifacts.init.registrate.ArtifactItemRegistry;
 import dev.xkmc.l2library.util.data.LootTableTemplate;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
@@ -13,6 +14,10 @@ public class ArtifactGLMProvider extends GlobalLootModifierProvider {
 
 	@Override
 	protected void start() {
-		this.add("health_based", new ArtifactLootModifier(LootTableTemplate.byPlayer().build()));
+		this.add("health_based_1", new ArtifactLootModifier(100, 200, 1, ArtifactItemRegistry.RANDOM[0].asStack(), LootTableTemplate.byPlayer().build()));
+		this.add("health_based_2", new ArtifactLootModifier(200, 300, 1, ArtifactItemRegistry.RANDOM[1].asStack(), LootTableTemplate.byPlayer().build()));
+		this.add("health_based_3", new ArtifactLootModifier(300, 400, 1, ArtifactItemRegistry.RANDOM[2].asStack(), LootTableTemplate.byPlayer().build()));
+		this.add("health_based_4", new ArtifactLootModifier(400, 500, 1, ArtifactItemRegistry.RANDOM[3].asStack(), LootTableTemplate.byPlayer().build()));
+		this.add("health_based_5", new ArtifactLootModifier(500, Integer.MAX_VALUE, 1, ArtifactItemRegistry.RANDOM[4].asStack(), LootTableTemplate.byPlayer().build()));
 	}
 }

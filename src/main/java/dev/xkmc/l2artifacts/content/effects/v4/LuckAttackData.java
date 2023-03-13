21c0346cd116ebcbf4bf0a8db8b4be0cb707e992
lcy0x1 <arthurlcy0x1@gmail.com>
2023-03-13 16:03:20 -0700

    bug fix

--- src/main/java/dev/xkmc/l2artifacts/content/effects/v4/LuckAttackData.java
+++ src/main/java/dev/xkmc/l2artifacts/content/effects/v4/LuckAttackData.java
@@ -3,7 +3,9 @@ package dev.xkmc.l2artifacts.content.effects.v4;
 import dev.xkmc.l2artifacts.content.effects.attribute.AttributeSetData;
 import dev.xkmc.l2library.serial.SerialClass;
 
+@SerialClass
 public class LuckAttackData extends AttributeSetData {
+
 	@SerialClass.SerialField
 	public int count;
 

b3a545ce85c26b94844fda73d011acff3365d34d
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-07-26 10:28:08 +0800

    multi page set screen

---
+++ src/main/java/dev/xkmc/l2artifacts/content/client/tab/SetEffectEntries.java
@@ -0,0 +1,64 @@
+package dev.xkmc.l2artifacts.content.client.tab;
+
+import com.mojang.datafixers.util.Pair;
+import dev.xkmc.l2artifacts.content.core.ArtifactSet;
+import dev.xkmc.l2artifacts.content.core.BaseArtifact;
+import dev.xkmc.l2library.util.code.TextWrapper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotResult;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public record SetEffectEntries(
+		ArtifactSet.SetContext set, int size,
+		List<Pair<List<FormattedCharSequence>, List<Component>>> text
+) {
+
+	public static List<List<SetEffectEntries>> aggregate(@Nullable LivingEntity le, int width, int linePerPage) {
+		if (le == null) return List.of();
+		var opt = CuriosApi.getCuriosInventory(le).resolve();
+		if (opt.isEmpty()) return List.of();
+		List<SlotResult> slots = opt.get()
+				.findCurios(stack -> stack.getItem() instanceof BaseArtifact);
+		List<SetEffectEntries> raw = new ArrayList<>();
+		for (SlotResult sr : slots) {
+			ItemStack stack = sr.stack();//玩家身上的饰品
+			BaseArtifact base = (BaseArtifact) stack.getItem();
+			var optSet = base.set.get().getCountAndIndex(sr.slotContext());
+			if (optSet.isEmpty()) continue;
+			var result = optSet.get();
+			if (result.current_index() > 0) continue;
+			List<Pair<List<FormattedCharSequence>, List<Component>>> ans = new ArrayList<>();
+			var list = base.set.get().addComponents(result);
+			int size = 0;
+			for (var pair : list) {
+				var lines = TextWrapper.wrapText(Minecraft.getInstance().font, pair.getFirst(), width - 16);
+				size += lines.size();
+				ans.add(Pair.of(lines, pair.getSecond()));
+			}
+			raw.add(new SetEffectEntries(result, size, ans));
+		}
+		List<List<SetEffectEntries>> ans = new ArrayList<>();
+		List<SetEffectEntries> current = new ArrayList<>();
+		int size = 0;
+		for (var e : raw) {
+			if (size > 0 && size + e.size() > linePerPage) {
+				ans.add(current);
+				current = new ArrayList<>();
+				size = 0;
+			}
+			size += e.size();
+			current.add(e);
+		}
+		if (!current.isEmpty()) ans.add(current);
+		return ans;
+	}
+
+}
--- src/main/java/dev/xkmc/l2artifacts/content/client/tab/SetEffectScreen.java
+++ src/main/java/dev/xkmc/l2artifacts/content/client/tab/SetEffectScreen.java
@@ -1,20 +1,17 @@
 package dev.xkmc.l2artifacts.content.client.tab;
 
-import dev.xkmc.l2artifacts.content.core.BaseArtifact;
 import dev.xkmc.l2artifacts.init.ArtifactClient;
 import dev.xkmc.l2library.util.Proxy;
-import dev.xkmc.l2library.util.code.TextWrapper;
+import dev.xkmc.l2tabs.init.data.L2TabsConfig;
 import dev.xkmc.l2tabs.tabs.contents.BaseTextScreen;
 import dev.xkmc.l2tabs.tabs.core.TabManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.SlotResult;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -23,18 +20,44 @@ import java.util.Optional;
 
 public class SetEffectScreen extends BaseTextScreen {
 
+	private final int page;
+
 	protected SetEffectScreen(Component title) {
+		this(title, 0);
+	}
+
+	protected SetEffectScreen(Component title, int page) {
 		super(title, new ResourceLocation("l2tabs:textures/gui/empty.png"));
+		this.page = page;
 	}
 
 	@Override
 	public void init() {
 		super.init();
 		new TabManager(this).init(this::addRenderableWidget, ArtifactClient.TAB_SET_EFFECTS);
+		Player player = Proxy.getClientPlayer();
+		var all = SetEffectEntries.aggregate(player, imageWidth - 16, L2TabsConfig.CLIENT.attributeLinePerPage.get());
+		int totalPage = Math.max(1, all.size());
+		int x = (this.width + this.imageWidth) / 2 - 16;
+		int y = (this.height - this.imageHeight) / 2 + 4;
+		int w = 10;
+		int h = 11;
+		if (this.page > 0) {
+			this.addRenderableWidget(Button.builder(Component.literal("<"), (e) -> {
+				this.click(-1);
+			}).pos(x - w - 1, y).size(w, h).build());
+		}
+
+		if (this.page < totalPage - 1) {
+			this.addRenderableWidget(Button.builder(Component.literal(">"), (e) -> {
+				this.click(1);
+			}).pos(x, y).size(w, h).build());
+		}
 	}
 
-	@Nullable
-	private List<Component> hover = null;
+	private void click(int btn) {
+		Minecraft.getInstance().setScreen(new SetEffectScreen(this.getTitle(), this.page + btn));
+	}
 
 	@Override
 	public void render(GuiGraphics g, int mx, int my, float ptick) {
@@ -42,34 +65,22 @@ public class SetEffectScreen extends BaseTextScreen {
 		Player player = Proxy.getClientPlayer();
 		int x = leftPos + 8;
 		int y = topPos + 6;
-		var opt = CuriosApi.getCuriosInventory(player).resolve();
-		if (opt.isEmpty()) return;
-		List<SlotResult> slots = opt.get()
-				.findCurios(stack -> stack.getItem() instanceof BaseArtifact);
+		var all = SetEffectEntries.aggregate(player, imageWidth - 16, L2TabsConfig.CLIENT.attributeLinePerPage.get());
 		List<FormattedCharSequence> seq = new ArrayList<>();
-		hover = null;
-		for (SlotResult sr : slots) {
-			ItemStack stack = sr.stack();//玩家身上的饰品
-			BaseArtifact base = (BaseArtifact) stack.getItem();
-			base.set.get().getCountAndIndex(sr.slotContext()).ifPresent(result -> {
-				if (result.current_index() > 0) {//第一件即可获取该套装全部信息
-					return;
+		List<Component> hover = null;
+		if (page < 0 || page >= all.size()) return;
+		for (var e : all.get(page)) {
+			for (var pair : e.text()) {
+				var lines = pair.getFirst();
+				int row = topPos + 6 + seq.size() * 10;
+				int column = leftPos + 6;
+				seq.addAll(lines);
+				int rowAft = topPos + 6 + seq.size() * 10;
+				int columnAft = leftPos + 6 + lines.size() * 45;
+				if (my >= row && my < rowAft && mx >= column && mx < columnAft) {
+					hover = pair.getSecond();
 				}
-
-				var list = base.set.get().addComponents(result);
-				for (var pair : list) {
-					var lines = TextWrapper.wrapText(Minecraft.getInstance().font, pair.getFirst(), imageWidth - 16);
-					int row = topPos + 6 + seq.size() * 10;
-					int column = leftPos + 6 ;
-					seq.addAll(lines);
-					int rowAft = topPos + 6 + seq.size() * 10;
-					int columnAft = leftPos + 6 + lines.size() *45 ;
-					if (my >= row && my < rowAft && mx >=column && mx <columnAft) {
-						hover = pair.getSecond();
-					}
-				}
-			});
-
+			}
 		}
 		for (var comp : seq) {
 			g.drawString(font, comp, x, y, 0, false);

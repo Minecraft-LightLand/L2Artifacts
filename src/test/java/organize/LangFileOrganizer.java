3ff0aebb3be470773529d315b04c2cdff4759ad7
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-10-15 10:09:49 +0800

    lang tmp

--- src/test/java/organize/LangFileOrganizer.java
+++ src/test/java/organize/LangFileOrganizer.java
@@ -8,6 +8,7 @@ import com.mojang.datafixers.util.Pair;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +31,7 @@ public class LangFileOrganizer extends ResourceOrganizer {
 			JsonObject dst_json = new JsonObject();
 			for (File fj : fi.listFiles()) {
 				if (!fj.getName().endsWith(".json")) continue;
-				JsonObject json = new JsonParser().parse(new FileReader(fj.getPath())).getAsJsonObject();
+				JsonObject json = new JsonParser().parse(new FileReader(fj.getPath(), StandardCharsets.UTF_8)).getAsJsonObject();
 				inject("", json, dst_json);
 				if (json.has("-cartesian")) {
 					JsonObject block_list = json.get("-cartesian").getAsJsonObject();
@@ -48,23 +49,31 @@ public class LangFileOrganizer extends ResourceOrganizer {
 										finalMap.add(Pair.of(ent1.getKey(), ent1.getValue().getAsString())));
 							} else {
 								map = map.stream().flatMap(ent1 -> vector.getAsJsonObject().entrySet().stream()
-												.map(ent2 -> Pair.of(ent1.getFirst() + con + ent2.getKey(),
-														reverse ? ent2.getValue().getAsString() + ent1.getSecond() :
-																ent1.getSecond() + ent2.getValue().getAsString())))
+												.map(ent2 -> connect(ent1, ent2, reverse, con)))
 										.collect(Collectors.toList());
 							}
 						}
 						for (Pair<String, String> pair : map) {
-							dst_json.addProperty(path + "." + pair.getFirst(), pair.getSecond());
+							dst_json.addProperty((path.isEmpty() ? "" : path + ".") + pair.getFirst(), pair.getSecond());
 						}
 
 					});
 				}
 			}
-			FileWriter w = new FileWriter(target);
+			FileWriter w = new FileWriter(target, StandardCharsets.UTF_8);
 			w.write(GSON.toJson(dst_json));
 			w.close();
 		}
+	}
+
+	private Pair<String, String> connect(Pair<String, String> ent1, Map.Entry<String, JsonElement> ent2, boolean reverse, String con) {
+		String k1 = ent1.getFirst();
+		String k2 = ent2.getKey();
+		String key = k2.contains("*") ? k2.replaceFirst("\\*", k1) : k1.isEmpty() ? k2 : k1 + con + k2;
+		String v1 = ent1.getSecond();
+		String v2 = ent2.getValue().getAsString();
+		String val = v2.contains("*") ? v2.replaceFirst("\\*", v1) : reverse ? v2 + v1 : v1 + v2;
+		return Pair.of(key, val);
 	}
 
 	private void inject(String path, JsonObject src, JsonObject dst) {
@@ -77,4 +86,5 @@ public class LangFileOrganizer extends ResourceOrganizer {
 			}
 		}
 	}
+
 }
